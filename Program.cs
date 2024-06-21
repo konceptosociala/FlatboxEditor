6e58f304363db58419654de70899c27de0b8f549
Koncepto Sociala <sanerhp.mail@gmail.com>
2024-06-21 23:58:36 +0300

    valid declarative

--- Program.cs
+++ Program.cs
@@ -1,19 +1,25 @@
 ﻿using System;
 using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.ReactiveUI;
+using Avalonia.Themes.Fluent;
+using Avalonia.Markup.Declarative;
 
 namespace FlatboxEditor;
 
 class Program
 {
-    [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args) {
+        var lifetime = new ClassicDesktopStyleApplicationLifetime { Args = args, ShutdownMode = ShutdownMode.OnLastWindowClose };
 
-    public static AppBuilder BuildAvaloniaApp()
-        => AppBuilder.Configure<App>()
+        AppBuilder.Configure<Application>()
             .UsePlatformDetect()
-            .WithInterFont()
-            .LogToTrace()
-            .UseReactiveUI();
+            .AfterSetup(b => b.Instance?.Styles.Add(new FluentTheme()))
+            .SetupWithLifetime(lifetime);
+
+        lifetime.MainWindow = new Window()
+            .Title("Flatbox Editor");
+    }
 }
+
